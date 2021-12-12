83c76c9d410d638839ea4dd5f16c353ef931ca5d
Maxime Froment <mfroment99@gmail.com>
2021-12-12 15:23:59 +0900

    2021/12

---
+++ y_2021/tests/test_12.py
@@ -0,0 +1,17 @@
+from y_2021.d_12 import *
+
+input_values_1 = parse_input(__file__, '1')
+input_values_2 = parse_input(__file__, '2')
+input_values_3 = parse_input(__file__, '3')
+
+
+def test_1():
+    assert solve_1(input_values_1) == 10
+    assert solve_1(input_values_2) == 19
+    assert solve_1(input_values_3) == 226
+
+
+def test_2():
+    assert solve_2(input_values_1) == 36
+    assert solve_2(input_values_2) == 103
+    assert solve_2(input_values_3) == 3509
