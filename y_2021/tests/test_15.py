3162b6785202d423fcd026a0ab95707b72016984
Maxime Froment <mfroment99@gmail.com>
2021-12-16 00:48:52 +0900

    2021/15 with cleaner code

--- y_2021/tests/test_15.py
+++ y_2021/tests/test_15.py
@@ -4,9 +4,12 @@ input_values = parse_input(__file__)
 output_nuply = parse_input(__file__, 'nuply_output')
 
 def test_1():
+    assert Sweep.solve_1(input_values) == 40  # only working because trivial case with no no up / left moves
     assert Clunky.solve_1(input_values) == 40
-
+    assert Cleaner.solve_1(input_values) == 40
 
 def test_2():
     assert nuply(input_values, 5) == output_nuply
+    assert Sweep.solve_2(input_values) == 315  # only working because trivial case with no no up / left moves
     assert Clunky.solve_2(input_values) == 315
+    assert Cleaner.solve_2(input_values) == 315
