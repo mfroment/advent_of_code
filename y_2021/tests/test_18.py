239ec0560036723fc14cecce463fb01b5a9d516f
Maxime Froment <mfroment99@gmail.com>
2021-12-25 13:13:19 +0900

    2021/18  Add tests

--- y_2021/tests/test_18.py
+++ y_2021/tests/test_18.py
@@ -4,13 +4,8 @@ input_values = parse_input(__file__)
 
 
 def test_1():
-    for v in input_values:
-        print(v)
-        # print(l2s(v))
-        # print(s2l(l2s(v)))
-        # print(s2l(process(l2s(v))))
-    assert solve_1(input_values) == None
+    assert solve_1(input_values) == 4140
 
 
 def test_2():
-    assert solve_2(input_values) == None
+    assert solve_2(input_values) == 3993
