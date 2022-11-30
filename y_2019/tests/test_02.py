f7bd18b1a09dd6c4cac2cec1bcf188b55b88b777
Maxime Froment <mfroment99@gmail.com>
2022-11-30 14:57:33 +0900

    2019/02

---
+++ y_2019/tests/test_02.py
@@ -0,0 +1,15 @@
+from y_2019.d_02 import *
+
+input_values = parse_input(__file__)
+
+
+def test_1():
+    process(input_values)
+    assert (input_values == [3500, 9, 10, 70,
+                             2, 3, 11, 0,
+                             99,
+                             30, 40, 50])
+
+
+def test_2():
+    assert solve_2(input_values) == None
