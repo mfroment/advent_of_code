7da1c35cbbd3054a0622e8cf6cec3a1aabd38c88
Maxime Froment <mfroment99@gmail.com>
2022-12-06 14:16:19 +0900

    2022/06

---
+++ y_2022/tests/test_06.py
@@ -0,0 +1,11 @@
+from y_2022.d_06 import *
+
+input_values = parse_input(__file__)
+
+
+def test_1():
+    assert solve_1(input_values) == None
+
+
+def test_2():
+    assert solve_2(input_values) == None
