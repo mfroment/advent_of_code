447a9c913db1826b5b6e66d583cdd77861a47e85
Maxime Froment <mfroment99@gmail.com>
2022-12-06 14:16:19 +0900

    2022/06

---
+++ y_2022/d_06.py
@@ -0,0 +1,31 @@
+import time
+import aoc.utils as aocu
+
+
+def parse_input(file=__file__, prefix=None):
+    sections = aocu.read_input(file, prefix)
+    return sections[0][0]
+
+
+def solve(values, length):
+    i = length
+    while len(set(values[i - length:i])) != length:
+        i += 1
+    return i
+
+
+def solve_1(values):
+    return solve(values, 4)
+
+
+def solve_2(values):
+    return solve(values, 14)
+
+
+if __name__ == "__main__":
+    input_values = parse_input()
+
+    start_time = time.time()
+    print(f"Part 1: {str(solve_1(input_values)):<30}{'(':>30}{time.time() - start_time:.3f}s)")
+    start_time = time.time()
+    print(f"Part 2: {str(solve_2(input_values)):<30}{'(':>30}{time.time() - start_time:.3f}s)")
