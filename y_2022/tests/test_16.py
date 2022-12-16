109161e4b5af2a5d7ae0e0d32c97e37834197148
Maxime Froment <mfroment99@gmail.com>
2022-12-16 19:16:53 +0900

    2022/16

---
+++ y_2022/tests/test_16.py
@@ -0,0 +1,10 @@
+from y_2022.d_16 import *
+
+tunnels, flows = parse_input(__file__)
+
+def test_1():
+    assert solve_1(tunnels, flows) == 1651
+
+
+def test_2():
+    assert solve_2(tunnels, flows) == 1707
