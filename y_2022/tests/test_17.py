54744cff38cbbbec9a87893aa128fead9235b63f
Maxime Froment <mfroment99@gmail.com>
2022-12-17 17:40:20 +0900

    2022/17

---
+++ y_2022/tests/test_17.py
@@ -0,0 +1,11 @@
+from y_2022.d_17 import *
+
+input_values = parse_input(__file__)
+
+
+def test_1():
+    assert solve_1(input_values) == 3068
+
+
+def test_2():
+    assert solve_2(input_values) == 1514285714288
