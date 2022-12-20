2ee0a12ca163aaa28de52f8a1c647ca9915edc82
Maxime Froment <mfroment99@gmail.com>
2022-12-20 22:50:45 +0900

    2022/19

---
+++ y_2022/tests/test_19.py
@@ -0,0 +1,12 @@
+from y_2022.d_19 import *
+
+input_values = parse_input(__file__)
+
+# Skipping those tests, they are very slow:
+
+# def test_1():
+#     assert solve_1(input_values) == 33
+#
+#
+# def test_2():
+#     assert solve_2(input_values) == 3472
