c893732dcfb40b1e2c2ed7a9ef8742bc24f105dc
Maxime Froment <mfroment99@gmail.com>
2025-12-10 17:39:14 +0900

    Add ruff and reformat consistently (cont.)

--- _init/cookiecutter/y_{{ cookiecutter.year }}/tests/test_{{ cookiecutter.day }}.py
+++ _init/cookiecutter/y_{{ cookiecutter.year }}/tests/test_{{ cookiecutter.day }}.py
@@ -5,8 +5,8 @@ input_values = parse_input(__file__)
 
 
 def test_1():
-    assert solve_1(input_values) == aocu.s2i('{{ cookiecutter.example_answer_a }}')
+    assert solve_1(input_values) == aocu.s2i("{{ cookiecutter.example_answer_a }}")
 
 
 def test_2():
-    assert solve_2(input_values) == aocu.s2i('{{ cookiecutter.example_answer_b }}')
+    assert solve_2(input_values) == aocu.s2i("{{ cookiecutter.example_answer_b }}")
